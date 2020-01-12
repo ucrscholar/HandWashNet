46161ae9861d613028b26ea7648b7ba4bdc43877
ucrscholar <52807898+ucrscholar@users.noreply.github.com>
2020-01-11 20:00:40 -0800

    modify

--- lstm/learningRandomDurationDifferentSectionLength.py
+++ lstm/learningRandomDurationDifferentSectionLength.py
@@ -47,20 +47,37 @@ session = InteractiveSession(config=config)
 tag = {0: 'NailWashLeft', 1: 'NailWashRight', 2: 'ThumbFingureWash', 3: 'ForeFingureWash'}
 inv_tag = {v: k for k, v in tag.items()}
 
-ROOTPATH = './VRandom1/'
-SAMPLESNUM = 3000
+ROOTPATH = './VRandom3/'
+SAMPLESNUM = 5000
 ITERATE = 1
 # configure problem
 SIZE = 50
-SHUFF=True
+SHUFF=False
 
 
+def drawImage(img):
+    f = pyplot.figure(figsize=(5, 5))
+    # create a grayscale subplot for each frame
+    ax = f.add_subplot(1, 1, 1)
+    ax.imshow(img, cmap='Greys')
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    pyplot.show()
 # generate damped sine wave in [0,1]
 def generate_DampedSin(period, i, decay, amplify=1):
     return [i / period, 0.5 + 0.5 * amplify * sin(2 * pi * i / period) * exp(-decay * i)]
 
+def generate_DampedSinDuration(period, i, decay, amplify=1):
+    if i > period*2:
+        return [-1, -1]
+    return [i / period, 0.5 + 0.5 * amplify * sin(2 * pi * i / period) * exp(-decay * i)]
 
 def generate_sin(period, i, decay=0, amplify=1):
+    return [i / period, 0.5 + 0.5 * amplify * sin(2 * pi * i / period)]
+
+def generate_sinDuration(period, i, decay=0, amplify=1):
+    if i>period:
+        return [-1,-1]
     return [i / period, 0.5 + 0.5 * amplify * sin(2 * pi * i / period)]
 
 
@@ -88,6 +105,32 @@ def generate_circle(period, i, decay=0, radius=1, x0=0, y0=0):
 
     return [x, y]
 
+def generate_circleDuration(period, i, decay=0, radius=1, x0=0, y0=0):
+    R = radius
+    x_0 = x0
+    y_0 = y0
+    d = uniform(0.01, 0.1)
+    # for t in range(0, 2 * pi, 0.01):
+    dd = list();
+    zz = list();
+    for t in range(1000):
+        t = t / 1000.0
+        x = (R * cos(2 * pi * t / period) + R) / 2 * R + x_0;
+        y = (R * sin(2 * pi * t / period) + R) / 2 * R + y_0;
+        dd.append(y)
+        zz.append(x)
+    miy = np.min(dd)
+    mix = np.min(zz)
+    may = np.max(dd)
+    max = np.max(zz)
+
+    if i>period:
+        return [-1,-1]
+
+    x = (R * cos(2 * pi * i / period) + R) / 2 * R + x_0;
+    y = (R * sin(2 * pi * i / period) + R) / 2 * R + y_0;
+
+    return [x, y]
 
 def generate_Heart(period, i, decay):
     x_0 = randint(0, 1)
@@ -117,6 +160,37 @@ def generate_Heart(period, i, decay):
 
     return [x, y]
 
+
+def generate_HeartDuration(period, i, decay):
+    x_0 = randint(0, 1)
+    y_0 = randint(0, 1)
+    d = uniform(0.01, 0.1)
+    t = i;
+    dd = list();
+    zz = list();
+    for t in range(1000):
+        t = t / 1000.0
+        t = 2 * pi * t / period
+        x = 16 * pow(sin(t), 3);
+        y = 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t);
+        dd.append(y)
+        zz.append(x)
+    miy = np.min(dd)
+    mix = np.min(zz)
+    may = np.max(dd)
+    max = np.max(zz)
+
+    if i>period:
+        return [-1,-1]
+
+    i = 2 * pi * i / period
+    x = 16 * pow(sin(i), 3);
+    y = 13 * cos(i) - 5 * cos(2 * i) - 2 * cos(3 * i) - cos(4 * i);
+
+    x = (x - mix) / (max - mix);
+    y = (y - miy) / (may - miy);
+
+    return [x, y]
 
 # generate input and output pairs of damped sine waves
 def generate_examplesX(length, n_patterns, output):
@@ -162,11 +236,12 @@ def next_frameSin(row, last_frame, column):
     if column > last_frame.shape[1] - 1:
         column = last_frame.shape[1] - 1
     # choose the row index for the next step
-    step = randint(lower, upper)
+    step = 0#randint(lower, upper)
     # copy the prior frame
     frame = last_frame.copy()
     # add the new step
-    frame[row, column] = 1
+    if row>=0 or column>=0:
+        frame[row, column] = 1
     return frame, step
 
 
@@ -183,7 +258,8 @@ def next_frameDampedSin(row, last_frame, column):
     # copy the prior frame
     frame = last_frame.copy()
     # add the new step
-    frame[row, column] = 1
+    if row >= 0 or column >= 0:
+        frame[row, column] = 1
     return frame, step
 
 
@@ -200,7 +276,8 @@ def next_frameDampedCircle(row, last_frame, column):
     # copy the prior frame
     frame = last_frame.copy()
     # add the new step
-    frame[row, column] = 1
+    if row >= 0 or column >= 0:
+        frame[row, column] = 1
     return frame, step
 
 
@@ -217,7 +294,8 @@ def next_frameDampedHeart(row, last_frame, column):
     # copy the prior frame
     frame = last_frame.copy()
     # add the new step
-    frame[row, column] = 1
+    if row >= 0 or column >= 0:
+        frame[row, column] = 1
     return frame, step
 
 
@@ -248,7 +326,7 @@ def build_frames(size, timeStep=0):
     for i in range(1, size):
         i = i / float(size)
         column, row = generate_sin(1, i, amplify=amplify)
-        frame = zeros((size, size))
+        # frame = zeros((size, size))
         frame, step = next_frameSin(int(row * size / xratio), frame, int(column * size / yratio))
         frames.append(frame)
         # labelA.append('NailWashLeft')
@@ -261,7 +339,7 @@ def build_frames(size, timeStep=0):
     for i in range(1, size):
         i = i / float(size)
         column, row = generate_DampedSin(0.5, i, 3, amplify=amplify)
-        frame = zeros((size, size))
+        # frame = zeros((size, size))
         frame, step = next_frameDampedSin(int(row * size / xratio), frame, int(column * size / yratio))
         frames.append(frame)
         # labelA.append('NailWashRight')
@@ -277,7 +355,7 @@ def build_frames(size, timeStep=0):
     for i in range(1, size):
         i = float(i) / float(size)
         column, row = generate_circle(1, i, 0.5, radius=radius, x0=x0, y0=y0)
-        frame = zeros((size, size))
+        # frame = zeros((size, size))
         frame, step = next_frameDampedCircle(int(row * size / xratio), frame, int(column * size / yratio))
         frames.append(frame)
         # labelA.append('ThumbFingureWash')
@@ -291,7 +369,7 @@ def build_frames(size, timeStep=0):
     for i in range(1, size):
         i = float(i) / float(size)
         column, row = generate_Heart(1, i, 0.5)
-        frame = zeros((size, size))
+        # frame = zeros((size, size))
         frame, step = next_frameDampedHeart(int(row * size / xratio), frame, int(column * size / yratio))
         frames.append(frame)
         # labelA.append('ForeFingureWash')
@@ -317,10 +395,39 @@ def GenNailLeft(size):
     for i in range(1, size):
         i = i / float(size)
         column, row = generate_sin(1, i, amplify=amplify)
-        frame = zeros((size, size))
+        #frame = zeros((size, size))
         frame, step = next_frameSin(int(row * size / xratio), frame, int(column * size / yratio))
         frames.append(frame)
         # labelA.append('NailWashLeft')
+    return frames, labelA
+
+
+def GenNailLeftDuration(size):
+    frames = list()
+    labelA = list()
+    frame = zeros((size, size))
+    step = randint(0, size - 1)
+    # decide if we are heading left or right
+    right = 1 if rand.random() < 0.5 else 0
+    col = 0 if right else size - 1
+    frame[step, col] = 0
+    frames.append(frame)
+
+    amplify = randint(5, 10) / 10.0
+    xratio = randint(1, 4)
+    yratio = randint(1, 4)
+    duration = randint(5,10)/10
+    labelA.append('NailWashLeft')
+    for i in range(1, size):
+        i = i / float(size)
+        column, row = generate_sinDuration(duration, i, amplify=amplify)
+        if column>=0 and row>=0:
+            frame = zeros((size, size))
+            frame, step = next_frameSin(int(row * size / xratio), frame, int(column * size / yratio))
+            frames.append(frame)
+
+        # labelA.append('NailWashLeft')
+    #drawImage(frame)
     return frames, labelA
 
 
@@ -336,12 +443,34 @@ def GenNailRight(size):
     for i in range(1, size):
         i = i / float(size)
         column, row = generate_DampedSin(0.5, i, 3, amplify=amplify)
-        frame = zeros((size, size))
+        #frame = zeros((size, size))
         frame, step = next_frameDampedSin(int(row * size / xratio), frame, int(column * size / yratio))
         frames.append(frame)
         # labelA.append('NailWashRight')
     return frames, labelA
 
+def GenNailRightDuration(size):
+    frames = list()
+    labelA = list()
+    frame = zeros((size, size))
+    frames.append(frame)
+    amplify = randint(5, 20) / 10.0
+    xratio = randint(1, 4)
+    yratio = randint(1, 4)
+    duration = randint(3,8)/10
+
+    labelA.append('NailWashRight')
+    for i in range(1, size):
+        i = i / float(size)
+        column, row = generate_DampedSinDuration(duration, i, 3, amplify=amplify)
+        if column >= 0 and row >= 0:
+            frame = zeros((size, size))
+            frame, step = next_frameDampedSin(int(row * size / xratio), frame, int(column * size / yratio))
+            frames.append(frame)
+
+        # labelA.append('NailWashRight')
+    #drawImage(frame)
+    return frames, labelA
 
 def GenThumbFinger(size):
     frames = list()
@@ -357,12 +486,37 @@ def GenThumbFinger(size):
     for i in range(1, size):
         i = float(i) / float(size)
         column, row = generate_circle(1, i, 0.5, radius=radius, x0=x0, y0=y0)
-        frame = zeros((size, size))
+        #frame = zeros((size, size))
         frame, step = next_frameDampedCircle(int(row * size / xratio), frame, int(column * size / yratio))
         frames.append(frame)
         # labelA.append('ThumbFingureWash')
     return frames, labelA
 
+
+def GenThumbFingerDuration(size):
+    frames = list()
+    labelA = list()
+    frame = zeros((size, size))
+    frames.append(frame)
+    radius = randint(5, 7) / 10
+    xratio = randint(1, 3)
+    yratio = randint(1, 3)
+    x0 = randint(2, 3) / 10
+    y0 = randint(2, 3) / 10
+    duration = randint(3,10)/10
+
+    labelA.append('ThumbFingureWash')
+    for i in range(1, size):
+        i = float(i) / float(size)
+        column, row = generate_circleDuration(duration, i, 0.5, radius=radius, x0=x0, y0=y0)
+        if column >= 0 and row >= 0:
+            frame = zeros((size, size))
+            frame, step = next_frameDampedCircle(int(row * size / xratio), frame, int(column * size / yratio))
+            frames.append(frame)
+            # labelA.append('ThumbFingureWash')
+
+    #drawImage(frame)
+    return frames, labelA
 
 def GenForeFinger(size):
     frames = list()
@@ -376,10 +530,33 @@ def GenForeFinger(size):
     for i in range(1, size):
         i = float(i) / float(size)
         column, row = generate_Heart(1, i, 0.5)
-        frame = zeros((size, size))
+        #frame = zeros((size, size))
         frame, step = next_frameDampedHeart(int(row * size / xratio), frame, int(column * size / yratio))
         frames.append(frame)
         # labelA.append('ForeFingureWash')
+    return frames, labelA
+
+def GenForeFingerDuration(size):
+    frames = list()
+    labelA = list()
+    frame = zeros((size, size))
+    frames.append(frame)
+    radius = randint(5, 7) / 10
+    xratio = randint(1, 3)
+    yratio = randint(1, 3)
+    duration = randint(3,10)/10
+
+    labelA.append('ForeFingureWash')
+    for i in range(1, size):
+        i = float(i) / float(size)
+        column, row = generate_HeartDuration(duration, i, 0.5)
+        if column >= 0 and row >= 0:
+            frame = zeros((size, size))
+            frame, step = next_frameDampedHeart(int(row * size / xratio), frame, int(column * size / yratio))
+            frames.append(frame)
+            # labelA.append('ForeFingureWash')
+
+    #drawImage(frame)
     return frames, labelA
 
 
@@ -420,10 +597,41 @@ def build_framesRandom(size, timeStep=0):
         rand.shuffle(res)
     for i in res:
         fa, la = my_list[res[i]](size)
+
         frames += fa
         labelA += la
 
     return frames, labelA
+
+
+# generate a sequence of frames of a dot moving across an image
+def build_framesRandomDuration(size, timeStep=0):
+    frames = list()
+    labelA = list()
+    labelB = list()
+    labelC = list()
+
+    my_list = [GenNailLeftDuration, GenNailRightDuration, GenThumbFingerDuration, GenForeFingerDuration]
+
+    res = [0, 1, 2, 3]
+    if SHUFF:
+        rand.shuffle(res)
+    for i in res:
+        fa, la = my_list[res[i]](size)
+        for _ in range(size-len(fa)):
+            frame = zeros((size, size))
+            fa.append(frame)
+        frames += fa
+        labelA += la
+
+    return frames, labelA
+
+
+
+
+
+# show the plot
+pyplot.show()
 
 
 def validateData():
@@ -529,6 +737,26 @@ def generate_examplesRandom(size, n_patterns):
     return X, labels
 
 
+# generate multiple sequences of frames and reshape for network input
+def generate_examplesRandomDuration(size, n_patterns):
+    X, y = list(), list()
+    for i in range(n_patterns):
+        # print("gen{}/{}".format(i,n_patterns))
+        frames, labels = build_framesRandomDuration(size)
+        code = np.array(labels)
+        label_encoder = LabelEncoder()
+        vec = label_encoder.fit_transform(code)
+
+        X.append(frames)
+        y.append(vec)
+    # resize as [samples, timesteps, width, height, channels]
+
+    X = array(X).reshape(n_patterns, len(X[0]), size, size, 1)
+    y = array(y).reshape(n_patterns, 4)
+    labels = to_categorical(y, 4)
+
+    return X, labels
+
 
 # define the model
 '''model = Sequential()
@@ -623,6 +851,32 @@ def fit2():
                           fileName2=ROOTPATH + 'acc.png')
 
 
+def fitRandomDuration():
+    model_filename = ROOTPATH + 'lstm_model_v1.h5'
+    callback_checkpoint = ModelCheckpoint(
+        model_filename,
+        verbose=1,
+        monitor='val_loss',
+        save_best_only=True,
+    )
+    # fit model
+    for i in range(ITERATE):
+        print('begin gen')
+
+        X, y = generate_examplesRandomDuration(SIZE, SAMPLESNUM)
+        print('begin fit{}/{}'.format(i, SAMPLESNUM))
+        #if path.exists(model_filename):
+        #    model.load_weights(model_filename)
+        history = model.fit(X, y, batch_size=32,
+                            epochs=1, validation_split=0.01,
+                            shuffle=False,
+                            workers=2,
+                            use_multiprocessing=True,
+                            callbacks=[callback_checkpoint])
+        plot_segm_history(history, metrics=['loss', 'val_loss'], fileName1=ROOTPATH + 'loss.png',
+                          fileName2=ROOTPATH + 'acc.png')
+
+
 # evaluate model
 def eval(X, y):
     loss, acc = model.evaluate(X, y, verbose=0)
@@ -663,8 +917,20 @@ def testRandom():
         pred(X, y)
 
 
+def testRandomDuration():
+    model.load_weights(ROOTPATH + 'lstm_model_v1.h5')
+    X, y = generate_examplesRandomDuration(SIZE, 5)
+    eval(X, y)
+
+    for i in range(10):
+        X, y = generate_examplesRandomDuration(SIZE, 1)
+        pred(X, y)
+
+
 if __name__ == "__main__":
     print("Executed when invoked directly")
     # test2();
-    fit2();
-    testRandom();
+    # fit2();
+    # testRandom();
+    fitRandomDuration();
+    testRandomDuration();
